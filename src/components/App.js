ac547c8865f6ef72f223c105ac2ce5868edf5487
Hilton1 <josehilton02@gmail.com>
2022-10-21 11:25:03 -0300

    Adding themes, GlobalStyles and Routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,21 @@
+import { ThemeProvider } from 'styled-components';
+import { BrowserRouter } from 'react-router-dom';
+
+import GlobalStyles from '../assets/styles/global';
+import darkTheme from '../assets/styles/themes/dark';
+// import lightTheme from '../assets/styles/themes/light';
+
+import Routes from '../Routes';
+
 function App() {
   return (
-    <h1>Hello World</h1>
+    <BrowserRouter>
+      <ThemeProvider theme={darkTheme}>
+        <GlobalStyles />
+
+        <Routes />
+      </ThemeProvider>
+    </BrowserRouter>
   );
 }
 
