53a1e9e1946874bab0e52ed2588f68f4660c7749
Hilton1 <josehilton02@gmail.com>
2022-11-04 00:58:14 -0300

    Adding info_country

--- src/pages/Countrie/index.js
+++ src/pages/Countrie/index.js
@@ -1,5 +1,5 @@
 import { Link } from 'react-router-dom';
-import { Container, Header } from './styles';
+import { Container, Header, Content } from './styles';
 
 export default function Countrie() {
   return (
@@ -12,6 +12,64 @@ export default function Countrie() {
           Back
         </Link>
       </Header>
+
+      <Content>
+        <div className="image_container">
+          <img src="https://www.gov.br/planalto/pt-br/conheca-a-presidencia/acervo/simbolos-nacionais/bandeira/bandeiragrande.jpg" alt="Brasil" />
+        </div>
+        <div className="info_country">
+          <h1>Brasil</h1>
+
+          <div className="description">
+            <div className="left_description">
+              <div className="info">
+                <b>Native Name:</b>
+                <p>Brasil</p>
+              </div>
+              <div className="info">
+                <b>Population:</b>
+                <p>206,135,893</p>
+              </div>
+              <div className="info">
+                <b>Region:</b>
+                <p>America</p>
+              </div>
+              <div className="info">
+                <b>Sub Region:</b>
+                <p>America</p>
+              </div>
+              <div className="info">
+                <b>Capital</b>
+                <p>Brasília</p>
+              </div>
+            </div>
+            <div className="right_description">
+              <div className="info">
+                <b>Top Level Domain:</b>
+                <p>.br</p>
+              </div>
+              <div className="info">
+                <b>Currencies:</b>
+                <p>Real</p>
+              </div>
+              <div className="info">
+                <b>Languages:</b>
+                <p>Português</p>
+              </div>
+            </div>
+          </div>
+
+          <div className="border_coutries_container">
+            <b>Border Countries:</b>
+            <Link to="/argentina">Argentina</Link>
+            <Link to="/argentina">Argentina</Link>
+            <Link to="/argentina">Argentina</Link>
+            <Link to="/argentina">Argentina</Link>
+            <Link to="/argentina">Argentina</Link>
+            <Link to="/argentina">Argentina</Link>
+          </div>
+        </div>
+      </Content>
     </Container>
   );
 }
--- src/pages/Countrie/styles.js
+++ src/pages/Countrie/styles.js
@@ -22,3 +22,49 @@ export const Header = styled.header`
     box-shadow: 0px 0px 7px rgba(0, 0, 0, 0.15);
   }
 `;
+
+export const Content = styled.div`
+  display: grid;
+  grid-template-columns: 1fr 1fr;
+
+  .image_container {
+    max-width: 560px;
+
+    img {
+      width: 100%;
+    }
+  }
+
+  .info_country {
+    h1 {
+      margin-bottom: 24px;
+    }
+
+    .description {
+      display: flex;
+      justify-content: space-between;
+      margin-bottom: 64px;
+
+      .info {
+        display: flex;
+        gap: 8px;
+        margin-bottom: 16px;
+      }
+    }
+
+    .border_coutries_container {
+      display: grid;
+      grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+      gap: 10px;
+
+      a {
+        padding: 4px 0;
+        background: ${({ theme }) => theme.colors.elements};
+        text-align: center;
+        border-radius: 4px;
+        text-decoration: none;
+        color: ${({ theme }) => theme.colors.text};
+      }
+    }
+  }
+`;
